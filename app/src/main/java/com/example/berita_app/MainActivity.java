119f711bc5f181954fb57764d02ac4bd2b09691d
LettyLatifah <lettylatifah15@gmail.com>
2021-11-24 22:10:02 +0700

    commit

--- app/src/main/java/com/example/berita_app/MainActivity.java
+++ app/src/main/java/com/example/berita_app/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.berita_app;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.berita_app.API.APIClient;
@@ -25,7 +37,10 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String country_id = "id";
+    public static final String language_id = "id";
     public static final String API_KEY = "762ca07378aa4145b832435604b871cc";
+
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private List<Article> articles = new ArrayList<>();
@@ -37,21 +52,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         recyclerView = findViewById(R.id.main_recyclerView);
         layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setNestedScrollingEnabled(false);
 
-        loadJson();
+        loadJson("");
     }
 
-    public void loadJson(){
+    public void loadJson(final String keyword){
         APIInterface apiInterface = APIClient.getApiClient().create(APIInterface.class);
 
-        String country = Utils.getCountry();
+//        String country = Utils.getCountry();
+        String language = Utils.getLanguage();
+
         Call<News> call;
-        call = apiInterface.getNews(country, API_KEY);
+        if(keyword.length()>0){
+            call = apiInterface.getNewsSearch(keyword, language_id, "publishedAt",API_KEY);
+        } else {
+            call = apiInterface.getNews(country_id, API_KEY);
+        }
+
+
 
         call.enqueue(new Callback<News>() {
             @Override
@@ -66,6 +90,9 @@ public class MainActivity extends AppCompatActivity {
                     adapter = new Adapter(articles, MainActivity.this);
                     recyclerView.setAdapter(adapter);
                     adapter.notifyDataSetChanged();
+
+                    initListener();
+
                 }else{
                     Toast.makeText(MainActivity.this, "No Result", Toast.LENGTH_SHORT).show();
                 }
@@ -80,4 +107,60 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    private void initListener(){
+
+        adapter.setOnItemClickListener(new Adapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                ImageView imageView = view.findViewById(R.id.img);
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+
+                Article article = articles.get(position);
+                intent.putExtra("url", article.getUrl());
+                intent.putExtra("title", article.getTitle());
+                intent.putExtra("img",  article.getUrlToImage());
+                intent.putExtra("date",  article.getPublishedAt());
+                intent.putExtra("source",  article.getSource().getName());
+                intent.putExtra("author",  article.getAuthor());
+
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint("Mencari Berita Terbaru....");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query.length() > 2){
+                    loadJson(query);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                loadJson(newText);
+                return false;
+            }
+        });
+        searchMenuItem.getIcon().setVisible(false, false);
+
+        return true;
+    }
 }
