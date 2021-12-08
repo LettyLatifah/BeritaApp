e2c7261446706116b8dad1604ab4b7d439359deb
LettyLatifah <lettylatifah15@gmail.com>
2021-12-08 12:45:42 +0700

    Commit

--- app/src/main/java/com/example/berita_app/About.java
+++ app/src/main/java/com/example/berita_app/About.java
@@ -10,37 +10,5 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class About extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setSelectedItemId(R.id.nav_about);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.nav_category:
-                        startActivity(new Intent(getApplicationContext(),Category.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.nav_home:
-                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.nav_favorite:
-                        startActivity(new Intent(getApplicationContext(),FavoriteActivity.class));
-                        overridePendingTransition(0,0);
-                        return true;
-
-                    case R.id.nav_about:
-                        return true;
-                }
-
-                return false;
-            }
-        });
-    }
 }
